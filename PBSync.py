41f66d6844b3b54cc0761dca558a24e22b35150d
Doğa Can Yanıkoğlu <doga.yanikoglu@esensi.com.tr>
2019-07-05 11:15:09 +0300

    Pushing today's work

--- PBSync.py
+++ PBSync.py
@@ -4,9 +4,11 @@ import os.path
 import os
 import xml.etree.ElementTree as ET
 import _winapi
+import time
 import sys
 import datetime
 from shutil import copy
+import errno, os, stat, shutil
 from shutil import rmtree
 
 # PBSync Imports
@@ -19,6 +21,7 @@ from colorama import Fore, Back, Style
 
 
 ### Globals
+git_user_name = ""
 expected_branch_name = "UE4GitPluginTest"
 ############################################################################
 
@@ -70,10 +73,13 @@ def CleanCache():
             pass
 
 def CheckGitCredentials():
+    global git_user_name
     output = subprocess.getoutput(["git", "config", "user.name"])
     if output == "":
         user_name = input("Please enter your Gitlab username: ")
         subprocess.call(["git", "config", "--global", "user.name", user_name])
+    else:
+        git_user_name = output
 
     output = subprocess.getoutput(["git", "config", "user.email"])
     if output == "":
@@ -84,20 +90,29 @@ def SyncFile(file_path):
     return subprocess.call(["git", "checkout", "HEAD", "--", file_path])
 
 def AbortMerge():
-    return subprocess.call(["git", "merge", "--abort"])
+    subprocess.call(["git", "merge", "--abort"])
 
 def CheckoutTheirs(file_path):
     return subprocess.call(["git", "checkout", file_path, "--theirs"])
 
 def CheckoutOurs(file_path):
-    return subprocess.call(["git", "checkout", file_path, "--ours"])
+    subprocess.call(["git", "checkout", file_path, "--ours"])
+    return True
+
+def GitAddFile(file_path):
+    subprocess.call(["git", "add", file_path])
 
 def RemoveFile(file_path):
     try:
         os.remove(file_path)
-    except:
-        LogWarning("Error while deleting file ", file_path)
-        return False
+    except Exception as e:
+        LogWarning(e)
+        os.chmod(file_path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+        try:
+            os.remove(file_path)
+        except Exception as e:
+            LogWarning(e)
+            pass
     return not os.path.isfile(file_path)
 
 def CheckCurrentBranchName():
@@ -108,68 +123,149 @@ def CheckCurrentBranchName():
         LogError("Current branch is not set as " + expected_branch_name + ". Please request help on #tech-support")
 
 def resolve_conflicts_and_pull():
+    global git_user_name
     output = subprocess.getoutput(["git", "pull"])
     backup_folder = 'Backup/' + datetime.datetime.now().strftime("%I%M%Y%m%d")
-
+    
     if "Automatic merge failed" in str(output):
-        AbortMerge()
+        output = subprocess.getoutput(["git", "status", "--porcelain"])
         LogWarning("Conflicts found with your non-pushed commits. Another developer made changes on the files listed below, and pushed them into the repository before you:")
-        file_list = []
+        print(output + "\n\n")
+        du_file_list = [] # Deleted by us files
+        ud_file_list = [] # Deleted by them files
+        uu_file_list = [] # both modified files
         for file_path in output.splitlines():
-            if file_path[0] == '\t':
-                stripped_filename = file_path.strip()
-                file_list.append(stripped_filename)
-                print(stripped_filename)
+            if file_path[0:2] == "DU":
+                # File is deleted by us, but someone pushed a new commit for this file
+                stripped_filename = file_path[3:]
+                du_file_list.append(stripped_filename)
+            elif file_path[0:2] == "UU":
+                # Both modified
+                stripped_filename = file_path[3:]
+                uu_file_list.append(stripped_filename)
+            elif file_path[0:2] == "UD":
+                # File is deleted by them, but we did some work on this file
+                stripped_filename = file_path[3:]
+                ud_file_list.append(stripped_filename)
 
-        # LogWarning("You should decide what to do with conflicted files", False)
-        # print("------------------\nGive an option as input to select actions per conflicted file")
-        # modified_file_exists = False
-        # for file_path in file_list:
-        #     LogWarning("\nConflicted File: " + file_path)
-        #     LogWarning("Keeping our version will also change the same file in the repository with ours after a successful push.")
-        #     action = input("[1] Keep our version and reflect changes to repository\n[2] Keep incoming version and backup our file\n[1/2 ?]: ")
+        LogWarning("You should decide what to do with each conflicted file", False)
+        print("------------------\nGive an option as input to select actions per conflicted file:")
+        for file_path in du_file_list:
+            LogWarning("\nConflicted File: " + file_path, False)
+            LogWarning("This file is deleted by us, but someone pushed a new version of the same file into repository.")
+            action = input("[1] Keep the file deleted and reflect changes to repository\n[2] Restore the file back\n[1/2 ?]: ")
 
-        #     if int(action) == 2:
-        #         file_backup_path = backup_folder + "/" + file_path[0:file_path.rfind("/")]
-        #         os.makedirs(file_backup_path)
-        #         copy(file_path, file_backup_path)
-        #         LogSuccess("Original file copied into: " + file_backup_path)
-        #         LogSuccess("You can use this file if you want to restore your own version of the file later")
-
-        #         LogSuccess("Updating the file to newest version...")
-        #         if CheckoutTheirs(file_path) == 0:
-        #             LogSuccess("Conflict resolved for " + file_path + " with their version of the file")
-        #         else:
-        #             AbortMerge()
-        #             LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            if int(action) == 2:
+                LogSuccess("Updating the file to newest version...")
+                if CheckoutTheirs(file_path) == 0:
+                    LogSuccess("Conflict resolved for " + file_path + " with their version of the file")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
             
-        #     elif int(action) == 1:
-        #         LogSuccess("Keeping our version of the file...")
-        #         modified_file_exists = True
-        #         if CheckoutOurs(file_path) == 0:
-        #             LogSuccess("Conflict resolved for " + file_path + " with our version of the file.")
-        #         else:
-        #             AbortMerge()
-        #             LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            elif int(action) == 1:
+                LogSuccess("Keeping the file removed...")
+                if RemoveFile(file_path) == True:
+                    LogSuccess("Conflict resolved for " + file_path + " by keeping the file removed.")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
             
-        #     else:
-        #         LogError("Incorrect option has given as input. Aborting...")
+            else:
+                AbortMerge()
+                LogError("Incorrect option has given as input. Aborting...")
 
-        # LogSuccess("All conflicts are resolved. Trying to add conflicted files...")
-        # for file_path in file_list:
-        #     if subprocess.call(["git", "add", file_path]) != 0:
-        #         AbortMerge()
-        #         LogError("Something went wrong while adding " + file_path + ". Aborting merge. Please request help on #tech-support")
-        #     LogSuccess("Added " + file_path)
-        
-        # subprocess.call(["git", "merge", "--continue"])
-        
-        # if not modified_file_exists:
-        #     subprocess.call(["git", "rebase"])
-        # else:
-        #     subprocess.call(["git", "push"])
+        for file_path in ud_file_list:
+            LogWarning("\nConflicted File: " + file_path, False)
+            LogWarning("This file is deleted on the repository, but you did some changes on this file on your local.")
+            action = input("[1] Keep the file deleted, and backup your own version\n[2] Push your version of the file into repository back\n[1/2 ?]: ")
 
-        # LogSuccess("\nSynchronization successful!")
+            if int(action) == 2:
+                LogSuccess("Keeping our version of the file...")
+                if CheckoutOurs(file_path) == 0:
+                    LogSuccess("Conflict resolved for " + file_path + " with our version of the file")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            
+            elif int(action) == 1:
+                LogSuccess("Keeping the file as removed...")
+
+                file_backup_path = backup_folder + "/" + file_path[0:file_path.rfind("/")]
+                try:
+                    os.makedirs(file_backup_path)
+                except:
+                    pass
+                copy(file_path, file_backup_path)
+                time.sleep(1)
+                LogSuccess("Original file copied into: " + file_backup_path)
+                LogSuccess("You can use this file if you want to restore your own version of the file later")
+
+                if RemoveFile(file_path) == True:
+                    LogSuccess("Conflict resolved for " + file_path + " by keeping the file removed.")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            
+            else:
+                AbortMerge()
+                LogError("Incorrect option has given as input. Aborting...")
+        
+        for file_path in uu_file_list:
+            LogWarning("\nConflicted File: " + file_path, False)
+            LogWarning("This file is modified by us, but someone pushed a new version of the same file into repository.")
+            action = input("[1] Keep our version and reflect changes to repository\n[2] Overwrite our file with the incoming version, and backup the original file\n[1/2 ?]: ")
+
+            if int(action) == 2:
+                if CheckoutOurs(file_path) == True:
+                    LogSuccess("Temporarily restored back our version for backup.")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to temporarily restore back our version of " + file_path + " for backup. Aborting merge. Please request help on #tech-support")
+                    
+                file_backup_path = backup_folder + "/" + file_path[0:file_path.rfind("/")]
+                try:
+                    os.makedirs(file_backup_path)
+                except:
+                    pass
+                copy(file_path, file_backup_path)
+                time.sleep(1)
+                LogSuccess("Original file copied into: " + file_backup_path)
+                LogSuccess("You can use this file if you want to restore your own version of the file later")
+
+                LogSuccess("Updating the file to newest version...")
+                if CheckoutTheirs(file_path) == 0:
+                    LogSuccess("Conflict resolved for " + file_path + " with their version of the file")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            
+            elif int(action) == 1:
+                LogSuccess("Keeping our version of the file...")
+                if CheckoutOurs(file_path) == True:
+                    LogSuccess("Conflict resolved for " + file_path + " with our version of the file.")
+                else:
+                    AbortMerge()
+                    LogError("Something went wrong while trying to resolve conflicts on " + file_path + ". Aborting merge. Please request help on #tech-support")
+            
+            else:
+                AbortMerge()
+                LogError("Incorrect option has given as input. Aborting...")
+
+        LogSuccess("All conflicts are resolved. Trying to add conflicted files...")
+        for file_path in (uu_file_list + du_file_list + ud_file_list):
+            GitAddFile(file_path)
+            LogSuccess("Added " + file_path)
+        
+        response = input("You're about to push the resulting commit into the repository, are you sure? [y/n]")
+        if response == "y" or response == "Y":
+            subprocess.call(["git", "commit", "-m", "Resolving merge conflict on " + git_user_name + "'s workspace"])
+            subprocess.call(["git", "push"])
+        else:
+            AbortMerge()
+            LogError("Merge aborted, changes are reverted. You need to fix your workspace before beginning working with Unreal Engine. Please request help on #tech-support")
+
+        LogSuccess("\nSynchronization successful, changes are reflected into repository!")
     elif "Aborting" in str(output):
         LogWarning("Conflicts found with uncommitted files in your workspace. Another developer made changes on the files listed below, and pushed them into the repository before you:")
         file_list = []
@@ -179,17 +275,22 @@ def resolve_conflicts_and_pull():
                 file_list.append(stripped_filename)
                 print(stripped_filename)
 
-        LogWarning("You should decide which files should be backed up", False)
+        LogWarning("You need to decide which files should be backed up", False)
        
-        print("------------------\nGive an option as input to select actions per conflicted file")
+        print("------------------\nGive an option as input to select actions per conflicted file:")
         for file_path in file_list:
             LogWarning("\nConflicted File: " + file_path)
             action = input("[1] Overwrite the file without backup\n[2] Overwrite the file with getting a backup of the current version\n[1/2 ?]: ")
 
             if int(action) == 2:
                 file_backup_path = backup_folder + "/" + file_path[0:file_path.rfind("/")]
-                os.makedirs(file_backup_path)
+                try:
+                    os.makedirs(file_backup_path)
+                except:
+                    # Probably a path already exists error, pass
+                    pass
                 copy(file_path, file_backup_path)
+                time.sleep(1)
                 LogSuccess("Original file copied into: " + file_backup_path)
                 LogSuccess("You can use this file if you want to restore your own version of the file later")
             elif int(action) != 1:
