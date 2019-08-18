795561a80c52b4622e0bc38e40f15444561db76e
Doğa Can Yanıkoğlu <doga.yanikoglu@esensi.com.tr>
2019-08-18 19:21:23 +0300

    Git 2.23 support

--- PBSync.py
+++ PBSync.py
@@ -19,7 +19,7 @@ from colorama import Fore, Back, Style
 
 ### Globals
 pbsync_version = "0.0.7"
-supported_git_version = "2.22.0"
+supported_git_version = "2.23.0"
 supported_lfs_version = "2.8.0"
 engine_base_version = "4.23"
 
