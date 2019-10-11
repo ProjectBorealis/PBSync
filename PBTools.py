59463fa326b8001e2b5cd0370a4c0437d35606c0
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2019-10-11 12:59:09 +0300

    Add error condition for DDC generator

--- PBTools.py
+++ PBTools.py
@@ -55,7 +55,9 @@ def generate_ddc_data():
         if os.path.isdir(installation_dir):
             ue_editor_executable = os.path.join(installation_dir, "Engine/Binaries/Win64/UE4Editor.exe")
             if os.path.isfile(ue_editor_executable):
-                subprocess.call([str(ue_editor_executable), os.path.join(os.getcwd(), PBConfig.get('uproject_path')), "-run=DerivedDataCache", "-fill"])
+                err = subprocess.call([str(ue_editor_executable), os.path.join(os.getcwd(), PBConfig.get('uproject_path')), "-run=DerivedDataCache", "-fill"])
+                if err == 3221225786:
+                    return False
                 return PBParser.ddc_update_version()
     
     return False
