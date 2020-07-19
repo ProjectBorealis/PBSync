a390321b0e516d8c0e3c4bac421bd5b15824fb04
mastercoms <mastercoms@tuta.io>
2020-07-19 11:59:27 -0400

    clear out outdated commented code

--- pbpy/pbtools.py
+++ pbpy/pbtools.py
@@ -258,10 +258,8 @@ def resolve_conflicts_and_pull(retry_count=0, max_retries=1):
         stashed = proc.returncode == 0 and "Saved working directory and index state" in out
         pblog.info(out)
         pblog.info("Trying to rebase workspace with the latest changes from the repository...")
-        result = run_with_combined_output(["git", "rebase", f"origin/{branch_name}", "--no-autostash"])
         # TODO: autostash handling
-        # pblog.info("Trying to rebase workspace with latest changes on the repository...")
-        # result = run_with_combined_output(["git", "pull", "--rebase", "--autostash"])
+        result = run_with_combined_output(["git", "rebase", f"origin/{branch_name}", "--no-autostash"])
         code = result.returncode
         out = result.stdout
         pblog.info(out)
