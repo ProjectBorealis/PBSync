840947db3aeb8453a9efc5678282c02dc9934cd8
mastercoms <mastercoms@tuta.io>
2020-11-01 12:00:40 -0500

    fix fstring

--- pbsync/pbsync.py
+++ pbsync/pbsync.py
@@ -252,7 +252,7 @@ def sync_handler(sync_val: str, repository_val=None, requested_bundle_name=None)
             pblog.info(f"Engine build {requested_bundle_name}-{engine_version} successfully registered")
             if pbconfig.get("is_ci"):
                 keep = 3
-                pblog.info("Keeping the last {keep} engine versions and removing the rest.")
+                pblog.info(f"Keeping the last {keep} engine versions and removing the rest.")
                 pbunreal.clean_old_engine_installations(keep=keep)
         else:
             pbtools.error_state(f"Something went wrong while registering engine build {requested_bundle_name}-{engine_version}")
