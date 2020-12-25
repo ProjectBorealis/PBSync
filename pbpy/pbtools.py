384b5fae3c36b320169f57d464fb9cac5bdf2f69
mastercoms <mastercoms@tuta.io>
2020-12-25 12:41:53 -0500

    fix lfs still downloading

--- pbpy/pbtools.py
+++ pbpy/pbtools.py
@@ -334,7 +334,7 @@ def resolve_conflicts_and_pull(retry_count=0, max_retries=1):
         pblog.info(out)
         pblog.info("Rebasing workspace with the latest changes from the repository...")
         # Get the latest files, but skip smudge so we can super charge a LFS pull as one batch
-        result = run_with_combined_output([pbgit.get_git_executable(), "-c", "filter.lfs.smudge=", "-c", "filter.lfs.required=false", "rebase", f"origin/{branch_name}", "--no-autostash"])
+        result = run_with_combined_output([pbgit.get_git_executable(), "-c", "filter.lfs.smudge=", "-c", "filter.lfs.process=", "-c", "filter.lfs.required=false", "rebase", f"origin/{branch_name}", "--no-autostash"])
         # Pull LFS in one go since we skipped smudge (faster)
         run([pbgit.get_lfs_executable(), "pull"])
         code = result.returncode
