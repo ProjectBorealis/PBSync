972a4dd4f5319e786cdf5fd6e0bafd10cff36ae5
mastercoms <mastercoms@tuta.io>
2021-08-22 15:08:00 -0400

    try to work out false positives

--- build.bat
+++ build.bat
@@ -1,10 +1,16 @@
 @echo off
 set PYTHONOPTIMIZE=1
 set PYTHONHASHSEED=0
+set PYI_STATIC_ZLIB=1
+set OBJECT_MODE=64
 pyinstaller --onefile ^
             --additional-hooks-dir=hooks ^
             -n PBSync ^
             --clean ^
+            --console ^
+            --icon=icon.ico ^
+            --runtime-tmpdir Saved ^
+            --key %random% ^
             -i resources/icon.ico ^
             --version-file version.rc ^
             -p pbpy:pbsync pbsync/__main__.py
