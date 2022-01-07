84f567cbc088f5af836b34ba1cc587cc57ea0297
mastercoms <mastercoms@tuta.io>
2022-01-07 08:00:32 -0500

    update dependencies

--- dependencies.bat
+++ dependencies.bat
@@ -1,3 +1,4 @@
 @echo off
 python -m pip install -U pip
+python -m pip install -U setuptools wheel
 pip install -Ur requirements.txt
--- dependencies.sh
+++ dependencies.sh
@@ -6,4 +6,5 @@ PATH="$HOME/.local/bin:$PATH"
 export PATH
 
 python -m pip install -U pip
+python -m pip install -U setuptools wheel
 pip install -Ur requirements-linux.txt
