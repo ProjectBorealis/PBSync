433de345dc67c7ab16bdb2f03b747d93c981ef85
TwentyPast4 <dan.toskan5@gmail.com>
2024-02-11 20:40:02 +0100

    Add branch support to steamcmd, similarly to how platform support works on butler

--- pbpy/pbsteamcmd.py
+++ pbpy/pbsteamcmd.py
@@ -8,7 +8,7 @@ def publish_build(branch_type, steamcmd_exec_path, publish_stagedir, app_script)
     pblog.error("steamcmd was not configured.")
     return False
 
-  script_path = (Path() / app_script).resolve()
+  script_path = (Path() / app_script.format(branch_type)).resolve()
 
   proc = pbtools.run([steamcmd_exec_path, "+login", pbconfig.get_user("steamcmd", "username"), pbconfig.get_user("steamcmd", "password"), "+run_app_build", script_path, "+quit"])
   result = proc.returncode
