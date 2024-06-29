9f0c8a2b670ac0e3f5df10be47d451cc7b94bd59
TwentyPast4 <dan.toskan5@gmail.com>
2024-06-29 13:33:54 +0200

    fix(steam): Path errors

--- pbpy/pbsteamcmd.py
+++ pbpy/pbsteamcmd.py
@@ -14,24 +14,25 @@ def publish_build(branch_type, steamcmd_exec_path, publish_stagedir, app_script,
   
   # if drm wrapping is configured
   if drm_app_id and drm_exe_path:
+    drm_exe_path = Path(drm_exe_path)
     if not Path.is_absolute(drm_exe_path):
       drm_exe_path = (Path(pbconfig.config_filepath).parent / drm_exe_path).resolve()
     if not Path.is_file(drm_exe_path):
       pblog.error("steamcmd/drm/targetbinary does not exist.")
       return False
     drm_command = base_steamcmd_command.copy()
-    drm_output = "wrappedBin" + Path(drm_exe_path).suffix # save file to wrappedBin.exe temporarily
-    drm_command.extend(["+drm_wrap", drm_exe_path, drm_output, "drmtoolp", "0", "+quit"]) # the drm wrap command https://partner.steamgames.com/doc/features/drm
+    drm_output = Path("wrappedBin" + drm_exe_path.suffix) # save file to wrappedBin.exe temporarily
+    drm_command.extend(["+drm_wrap", str(drm_exe_path), str(drm_output), "drmtoolp", "0", "+quit"]) # the drm wrap command https://partner.steamgames.com/doc/features/drm
     pblog.info("Wrapping game with steamworks DRM...")
     drm_proc = pbtools.run_with_output(drm_command)
-    pbtools.remove_file(drm_exe_path) # remove original file in any case
+    pbtools.remove_file(str(drm_exe_path)) # remove original file in any case
     if drm_proc.returncode != 0:
       pblog.error("Drm wrapping failed(%d)" % drm_proc.returncode)
       if Path.exists(drm_output):
-        pbtools.remove_file(drm_output)
+        pbtools.remove_file(str(drm_output))
       return False
     
-    shutil.move(drm_output, drm_exe_path) # move drm-wrapped file to location of original
+    shutil.move(str(drm_output), str(drm_exe_path)) # move drm-wrapped file to location of original
   
   script_path = (Path() / app_script.format(branch_type)).resolve()
   build_cmd = base_steamcmd_command.copy()
