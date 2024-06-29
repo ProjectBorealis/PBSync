787c0dee5174a897a4ee052ab2b8e02e53fc9997
TwentyPast4 <dan.toskan5@gmail.com>
2024-06-29 12:12:27 +0200

    tweak(steam): Do DRM wrap in two steps

--- pbpy/pbsteamcmd.py
+++ pbpy/pbsteamcmd.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import shutil
 
 from pbpy import pblog, pbtools, pbconfig
 
@@ -14,11 +15,22 @@ def publish_build(branch_type, steamcmd_exec_path, publish_stagedir, app_script,
   if drm_app_id and drm_exe_path:
     if not Path.is_absolute(drm_exe_path):
       drm_exe_path = (Path(pbconfig.config_filepath).parent / drm_exe_path).resolve()
-    if not Path.exists(drm_exe_path):
+    if not Path.is_file(drm_exe_path):
       pblog.error("steamcmd/drm/targetbinary does not exist.")
       return False
-    command.extend(["+drm_wrap", drm_exe_path, drm_exe_path, "drmtoolp", "0"])
-    pblog.info("Will use steamworks DRM")
+    drm_command = command.copy()
+    drm_output = "wrappedBin" + Path(drm_exe_path).suffix
+    drm_command.extend(["+drm_wrap", drm_exe_path, drm_output, "drmtoolp", "0", "+quit"])
+    pblog.info("Wrapping game with steamworks DRM...")
+    drm_proc = pbtools.run_with_output(drm_command)
+    if drm_proc.returncode != 0:
+      pblog.error("Drm wrapping failed(%d)" % drm_proc.returncode)
+      if Path.exists(drm_output):
+        pbtools.remove_file(drm_output)
+      return False
+    
+    shutil.move(drm_output, drm_exe_path)
+    return True
   
   command.extend(["+run_app_build", script_path, "+quit"])
   proc = pbtools.run(command)
