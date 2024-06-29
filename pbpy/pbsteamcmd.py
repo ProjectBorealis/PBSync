a52b172739681eedb06f400ec6041d10241d21a7
TwentyPast4 <dan.toskan5@gmail.com>
2024-06-29 12:21:35 +0200

    tweak(steam): Remove drm wrapped file after build

--- pbpy/pbsteamcmd.py
+++ pbpy/pbsteamcmd.py
@@ -23,15 +23,20 @@ def publish_build(branch_type, steamcmd_exec_path, publish_stagedir, app_script,
     drm_command.extend(["+drm_wrap", drm_exe_path, drm_output, "drmtoolp", "0", "+quit"])
     pblog.info("Wrapping game with steamworks DRM...")
     drm_proc = pbtools.run_with_output(drm_command)
+    pbtools.remove_file(drm_exe_path) # remove original file in any case
     if drm_proc.returncode != 0:
       pblog.error("Drm wrapping failed(%d)" % drm_proc.returncode)
       if Path.exists(drm_output):
         pbtools.remove_file(drm_output)
       return False
     
-    shutil.move(drm_output, drm_exe_path)
+    shutil.move(drm_output, drm_exe_path) # move drm-wrapped file to location of original
   
   command.extend(["+run_app_build", script_path, "+quit"])
   proc = pbtools.run(command)
   result = proc.returncode
+  
+  if Path.is_file(drm_exe_path):
+    pbtools.remove_file(drm_exe_path) # remove drm wrapped file, so that a subsequent build will re-build a non-wrapped version
+  
   return result
