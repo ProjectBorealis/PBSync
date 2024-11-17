e5e790dc36dca74736f8e2b14b4d1ea51060bbc5
mastercoms <mastercoms@tuta.io>
2024-11-17 16:27:47 -0500

    fix args bug

--- pbpy/pbunreal.py
+++ pbpy/pbunreal.py
@@ -1173,17 +1173,18 @@ def clear_cook_cache():
 def build_shaders(platform: str = "PCD3D_SM6"):
     uproject_path = get_uproject_path()
     project_name = uproject_path.stem
-    project_path = uproject_path.parent
+    project_path = str(uproject_path.parent)
+    args = [
+        str(get_editor_path()),
+        str(get_uproject_path()),
+        "-run=ShaderPipelineCacheTools",
+        "expand",
+        f"{project_path}/Saved/StagedBuilds/Windows/{project_name}/Saved/CollectedPSOs/*.rec.upipelinecache",
+        f"{project_path}/Saved/Shaders/{platform}/*.shk",
+        f"{project_path}/Build/Windows/PipelineCaches/PSO_{project_name}_{platform}.spc",
+    ]
     pbtools.run_stream(
-        [
-            get_editor_path(),
-            get_uproject_path(),
-            "-run=ShaderPipelineCacheTools",
-            "expand",
-            f"{project_path}/Saved/StagedBuilds/Windows/{project_name}/Saved/CollectedPSOs/*.rec.upipelinecache"
-            f"{project_path}/Saved/Shaders/{platform}/*.shk",
-            f"{project_path}/Build/Windows/PipelineCaches/PSO_{project_name}_{platform}.spc",
-        ],
+        args,
         logfunc=lambda x: pbtools.checked_stream_log(
             x, error="Error: ", warning="Warning: "
         ),
